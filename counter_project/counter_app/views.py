1d919350bf944e9f098de8a5b662946a510ace50
masharqah <59175914+masharqah@users.noreply.github.com>
2023-06-09 18:47:45 +0300

    Add files via upload

---
+++ counter_project/counter_app/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render, redirect
+def index(request):
+    count=request.session.get('counter', 0)
+    
+    if request.method == 'POST':
+        if 'increment' in request.POST:
+            increment = int(request.POST.get('increment'))
+            count += increment
+            request.session['counter'] = count
+        elif 'reset' in request.POST:
+            count = 0
+            request.session['counter'] = count
+        return redirect('/')
+
+    return render(request, 'index.html', {'count': count})
